3fa97225a09603677a73f48bc9253986770ab5da
Ben Carney <ben@carnivorous.org>
2013-04-07 13:46:32 -0700

    Basic controlls snapshot

--- lib/application.rb
+++ lib/application.rb
@@ -1,3 +1,4 @@
+require 'io/console'
 require_relative 'sprite_factory'
 require_relative 'world_generator'
 require_relative 'render_engine'
@@ -6,3 +7,13 @@ terrain = SpriteFactory.new(yaml: 'objects/terrain.yml')
 world = WorldGenerator.new(terrain, size: 200)
 re = RenderEngine.new(world.map)
 
+@x = @y = 0
+while true
+  re.render_chunk(x: @x, y: @y, size: 40)
+  ch = STDIN.getch
+  return if ch == "x"
+  @x += 1 if ch == "l"
+  @x -= 1 if ch == "h"
+  @y += 1 if ch == "j"
+  @y -= 1 if ch == "k"
+end
--- lib/render_engine.rb
+++ lib/render_engine.rb
@@ -1,11 +1,17 @@
 class RenderEngine
 
   def initialize(map)
+    @map = map
+  end
+
+  def render_chunk(args={})
+    args = {size: 8, x: 0, y: 0}.merge(args)
+
     t = Time.now
-    #print "\e[2J\e[f""]]"
-    map.each do |row|
-      row.each do |sprite|
-         print sprite.draw
+    print "\e[2J\e[f"
+    (args[:y]..(args[:y] + args[:size])).each do |col|
+      (args[:x]..(args[:x] + args[:size])).each do |row|
+        print @map[row][col].draw
       end
       puts
     end
